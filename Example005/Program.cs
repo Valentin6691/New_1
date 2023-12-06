906a4bfc1033ba82d281a09b180b613c3be0dfe4
Valentin <nitnelav6691@gmail.cov>
2023-12-06 19:37:33 +0300

    Маша

---
+++ Example005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Маша, ты коза?");
+}
+else{
+    Console.Write(username);
+    Console.Write(", ты козел?");
+}
