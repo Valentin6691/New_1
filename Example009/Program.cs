97360faf404303c051b57222b34e08798db63dce
Valentin <nitnelav6691@gmail.cov>
2023-12-06 22:39:29 +0300

    Example009

---
+++ Example009/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Ведите ваше число");
+int number = Convert.ToInt32(Console.ReadLine());
+int negnumber = number *-1;
+
+while(negnumber <= number)
+{
+    Console.WriteLine(negnumber);
+    negnumber = negnumber + 1;
+}
