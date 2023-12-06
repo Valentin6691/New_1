0bd09ef3ec8353c7ebb8a2b45d5f2d7a16943b92
Valentin <nitnelav6691@gmail.cov>
2023-12-07 00:17:56 +0300

    Test002

---
+++ Test002/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Ведите ваше число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите ваше число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите ваше число");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if(b > max)
+{
+max = b;
+}
+else if(c > max)
+{
+    max = c;
+}
+else
+{
+    max = a;
+}
+Console.WriteLine(max);
