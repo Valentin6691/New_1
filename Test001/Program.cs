566bcff72a3b74187314bd09922fc2ac0acbc37a
Valentin <nitnelav6691@gmail.cov>
2023-12-06 23:49:10 +0300

    Test001

---
+++ Test001/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Ведите ваше число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите ваше число");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if(firstNumber < secondNumber)
+      {
+        Console.WriteLine("Первое число `" + firstNumber + "` меньше чем второе число `" + secondNumber + "`");
+      }
+      else if(firstNumber > secondNumber)
+      {
+        Console.WriteLine("Первое число `" + firstNumber + "` больше чем второе число `" + secondNumber + "`");
+      }
+        else if(firstNumber == secondNumber)
+      {
+        Console.WriteLine("Введенные числа равны `" + secondNumber + "`");
+      }
