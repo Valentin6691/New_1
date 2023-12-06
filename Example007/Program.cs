44656801c8c52a262ef8e73d752d125250c46ea5
Valentin <nitnelav6691@gmail.cov>
2023-12-06 21:17:33 +0300

    Example007

---
+++ Example007/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Ведите ваше число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите ваше число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA == numberB*numberB)
+{
+    Console.WriteLine("Да");
+}
+else{
+    Console.WriteLine("Нет");
+}
