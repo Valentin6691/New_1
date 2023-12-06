3e1dfbc14df255ec3957de3f1497f2fc26079664
Valentin <nitnelav6691@gmail.cov>
2023-12-07 00:34:51 +0300

    Test003

---
+++ Test003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Ведите ваше число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number%2;
+if(result == 0)
+{
+    Console.WriteLine("Число `" + number + "` четное");
+}
+else
+{
+  Console.WriteLine("Число `" + number + "` нечетное");  
+}
