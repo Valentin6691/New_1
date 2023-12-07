8a3499f71ba20d6240e37022d85b453504b7d92a
Valentin <nitnelav6691@gmail.cov>
2023-12-07 08:43:26 +0300

    Test004

---
+++ Test004/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Ведите ваше число");
+int number = Convert.ToInt32(Console.ReadLine());
+int number_ = 2;
+while(number_ <= number)
+{
+Console.WriteLine(number_);
+number_ = number_ + 2;
+}
